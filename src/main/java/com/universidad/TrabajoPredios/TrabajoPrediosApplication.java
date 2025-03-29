e5a0e08d9c391fe0dad4f63d4d87286364c75ddb
Juan Manuel Bolivar Mojica <jbolivarm@ucentral.edu.co>
2025-03-28 23:32:04 -0200

    Inicio de Proyecto Predios

---
+++ src/main/java/com/universidad/TrabajoPredios/TrabajoPrediosApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.TrabajoPredios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPrediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPrediosApplication.class, args);
+	}
+
+}
