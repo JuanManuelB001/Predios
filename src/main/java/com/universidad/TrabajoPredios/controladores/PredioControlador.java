17a44c3d5ed25fcaa727c90ba93d806d16e16299
Juan Manuel Bolivar Mojica <jbolivarm@ucentral.edu.co>
2025-03-29 00:25:18 -0200

    Agregando Post

---
+++ src/main/java/com/universidad/TrabajoPredios/controladores/PredioControlador.java
@@ -0,0 +1,28 @@
+package com.universidad.TrabajoPredios.controladores;
+
+
+import com.universidad.TrabajoPredios.persistencia.entidades.Predios;
+import com.universidad.TrabajoPredios.servicios.ServicioRepositorio;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/predios")
+@AllArgsConstructor
+public class PredioControlador {
+    ServicioRepositorio servicioRepo;
+    /*
+    @GetMapping("/")
+    public List<Predios> listar(){
+        return servicioRepo.mostrarTodos();
+    }
+
+     */
+
+    @PostMapping("/")
+    public Predios agregar(@RequestBody Predios predio){
+        return servicioRepo.agregar(predio);
+    }
+}
